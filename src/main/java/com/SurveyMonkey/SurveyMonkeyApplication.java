2babf116f859372e3e9936644cc3ceabb0b4a3ec
Kevin Geng <kevin.geng@quarksys.ca>
2022-03-08 13:52:48 -0500

    add initialzr code

---
+++ src/main/java/com/SurveyMonkey/SurveyMonkeyApplication.java
@@ -0,0 +1,13 @@
+package com.SurveyMonkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyMonkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyMonkeyApplication.class, args);
+	}
+
+}
