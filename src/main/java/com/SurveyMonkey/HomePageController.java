12884bd481b56d7cc55c3415fa0e54e18a8f1a91
Blake <blake_db99@outlook.com>
2022-03-09 18:44:37 -0500

    Created Taker Home Page

---
+++ src/main/java/com/SurveyMonkey/HomePageController.java
@@ -0,0 +1,13 @@
+package com.SurveyMonkey;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping("/generalPage")
+    public String displaySurveyApp(){
+        return "generalPage";
+    }
+}
