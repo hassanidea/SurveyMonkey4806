a8967aac64482c93b00e70ce057896ea956db40a
OmarAgamy9 <omaragamy@cmail.carleton.ca>
2022-03-10 23:49:11 -0500

    Revert "Added the Surveyor Creation (UI, and Controller, and API config) and set up Ajax"
    This reverts commit 762be8f08600bfb2b672a5d22c2349ae3f691e44.

---
+++ src/main/java/com/SurveyMonkey/HomePageController.java
@@ -0,0 +1,15 @@
+package com.SurveyMonkey;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomePageController {
+
+    @RequestMapping("/generalPage")
+    public String startProgram() {
+        return "generalPage";
+    }
+}
+
