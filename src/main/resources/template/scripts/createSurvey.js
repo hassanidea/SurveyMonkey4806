043306e62cb7a9fad56c2695d8fb65e08eca6754
Hassan Hassan <hasnnasrbadr49@gmail.com>
2022-03-10 21:41:21 -0500

    Added UI and javascript for creating the survey

--- src/main/resources/template/scripts/createSurvey.js
+++
@@ -1,8 +0,0 @@
-
-function addQuestion() {
-          document.getElementById("addQuestion").insertAdjacentHTML("afterend",
-                "<form class='selectQuestion'> <h4>Select type of Question</h4><br><select name='questionTypes' id=questionTypes'><option value='multipleChoice'>Multiple Choice Question</option><option value='dropDown'>Drop Down Question</option>
-                  </select>");
-
-                  document.getElementById("addQuestion").insertAdjacentHTML("afterend",
-                                  "<form class='selectQuestion'><h4>Select type of Question</h4><br><select name='questionTypes' id='questionTypes'><option value='multipleChoice'>Multiple Choice Question</option><option value='dropDown'>Drop Down Question</option></select><button type='button'>Remove question</button></form>");
