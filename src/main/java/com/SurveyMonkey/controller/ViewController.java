ac4588dbcfe765fe29d255784f5273712a770796
OmarAgamy9 <omaragamy@cmail.carleton.ca>
2022-03-11 07:42:08 -0500

    Fixed naming structures

--- src/main/java/com/SurveyMonkey/controller/ViewController.java
+++ src/main/java/com/SurveyMonkey/controller/ViewController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ViewController {
 
-	@GetMapping("/home")
+	@GetMapping("/createSurveyQuestion")
 	public String home() {
-		return "home";
+		return "createSurveyQuestion";
 	}
 
 }
