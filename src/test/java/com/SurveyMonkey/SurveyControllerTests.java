cd85227aac5d512c19da75d84a61cae569a6b6a5
Blake <blake_db99@outlook.com>
2022-03-24 23:11:30 -0400

    Added survey results page layout. Added tests for HTTP and SurveyController

---
+++ src/test/java/com/SurveyMonkey/SurveyControllerTests.java
@@ -0,0 +1,19 @@
+package com.SurveyMonkey;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.SurveyMonkey.controller.SurveyController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class SurveyControllerTests {
+
+    @Autowired
+    private SurveyController surveyController;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(surveyController).isNotNull();
+    }
+}
