f94267a4aacc54a7b2f38f48fda57dcd63da4a67
OmarAgamy9 <omaragamy@cmail.carleton.ca>
2022-03-30 15:04:16 -0400

    Added testing MCQ Question

---
+++ src/main/java/test/model/MultipleChoiceQuestionTest.java
@@ -0,0 +1,45 @@
+package test;
+
+import com.SurveyMonkey.model.MultipleChoiceQuestionModel;
+
+import static org.junit.Assert.*;
+import java.util.*;
+
+public class MultipleChoiceQuestionTest {
+
+    MultipleChoiceQuestionModel mcQuestion = new MultipleChoiceQuestionModel();
+
+    @org.junit.Test
+    public void addQuestion() {
+        mcQuestion.setSurveyQuestion("Test");
+        assertEquals("Test",mcQuestion.getSurveyQuestion());
+    }
+
+    @org.junit.Test
+    public void addFirstChoice() {
+        mcQuestion.setChoiceOne("Test 1");
+        assertEquals("Test 1",mcQuestion.getChoiceOne());
+    }
+
+    @org.junit.Test
+    public void addSecondChoice() {
+        mcQuestion.setChoiceTwo("Test 2");
+        assertEquals("Test 2",mcQuestion.getChoiceOne());
+    }
+
+    @org.junit.Test
+    public void addThirdChoice() {
+        mcQuestion.setChoiceOne("Test 3");
+        assertEquals("Test 3",mcQuestion.getChoiceOne());
+    }
+
+    @org.junit.Test
+    public void addFourthChoice() {
+        mcQuestion.setChoiceOne("Test 4");
+        assertEquals("Test 4",mcQuestion.getChoiceOne());
+    }
+
+
+
+
+}
